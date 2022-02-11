fe66519b1dbeebd406dc94868a15d859ab7e6dc4
Adrian Witaszak <adrianwitaszak@gmail.com>
2022-02-11 21:10:56 +0000

    added contact page

--- src/jsMain/kotlin/com/adwi/site/pages/Contact.kt
+++ src/jsMain/kotlin/com/adwi/site/pages/Contact.kt
@@ -1,7 +1,6 @@
 package com.adwi.site.pages
 
 import androidx.compose.runtime.Composable
-import com.adwi.site.components.composables.EmSpacer
 import com.adwi.site.components.layouts.PageLayout
 import com.adwi.site.components.sections.ContactForm
 import com.adwi.site.components.sections.LogoHeader
@@ -11,8 +10,13 @@ import com.adwi.site.components.utils.Strings
 import com.varabyte.kobweb.compose.ui.Modifier
 import com.varabyte.kobweb.compose.ui.modifiers.color
 import com.varabyte.kobweb.compose.ui.modifiers.fontSize
+import com.varabyte.kobweb.compose.ui.modifiers.margin
 import com.varabyte.kobweb.compose.ui.modifiers.maxWidth
 import com.varabyte.kobweb.core.Page
+import com.varabyte.kobweb.silk.components.layout.breakpoint.displayIf
+import com.varabyte.kobweb.silk.components.style.ComponentStyle
+import com.varabyte.kobweb.silk.components.style.breakpoint.Breakpoint
+import com.varabyte.kobweb.silk.components.style.toModifier
 import com.varabyte.kobweb.silk.components.text.Text
 import org.jetbrains.compose.web.css.em
 import org.jetbrains.compose.web.css.px
@@ -24,17 +28,30 @@ fun ContactPage() {
         currentPage = NavSections.CONTACT
     ) {
         LogoHeader()
-        EmSpacer(4.0)
         Text(
             text = Strings.Contact.howCanIHelp,
-            modifier = Modifier
-                .maxWidth(800.px)
-                .color(Colors.darkGray)
-                .fontSize(3.em)
-//                .fontWeight(FontWeight.Lighter)
+            modifier = ContactHeaderStyle.toModifier()
         )
-        EmSpacer(4.0)
         ContactForm()
-        EmSpacer(4.0)
+    }
+}
+
+val ContactHeaderStyle = ComponentStyle("contact-header-style") {
+    base {
+        Modifier
+            .maxWidth(800.px)
+            .color(Colors.darkGray)
+            .fontSize(1.5.em)
+            .margin(leftRight = .5.em, topBottom = 2.em)
+    }
+    Breakpoint.SM {
+        Modifier
+            .fontSize(2.em)
+            .margin(leftRight = .5.em, topBottom = 2.em)
+    }
+    Breakpoint.LG {
+        Modifier
+            .fontSize(3.em)
+            .margin(leftRight = 1.em, topBottom = 3.em)
     }
 }
